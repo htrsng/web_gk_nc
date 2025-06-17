9985ba90012b756c5cade2fb2db5fdb04f0fe4f9
htrsng <tranghuyen20051312@gmail.com>
2025-06-17 13:07:31 +0700

    .

--- resources/views/dashboard.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container mx-auto p-4">
-    <h1 class="text-2xl font-bold">Admin Dashboard</h1>
-    <p class="mt-4">Welcome to the admin panel!</p>
-</div>
-@endsection
